0776590c9b688f39705a20fa43de931849d22e51
Amir Bar <amir.bar@cs.tau.ac.il>
2022-02-15 11:30:00 -0800

    add lr_drop schedule

--- configs/DETReg_fine_tune_1pct_coco.sh
+++ configs/DETReg_fine_tune_1pct_coco.sh
@@ -5,4 +5,4 @@ set -x
 EXP_DIR=exps/DETReg_fine_tune_1pct_coco
 PY_ARGS=${@:1}
 
-python -u main.py --output_dir ${EXP_DIR} --filter_pct 0.01 --dataset coco --pretrain exps/DETReg_top30_coco/checkpoint.pth --epochs 2000 ${PY_ARGS}
+python -u main.py --output_dir ${EXP_DIR} --filter_pct 0.01 --dataset coco --pretrain exps/DETReg_top30_coco/checkpoint.pth --epochs 2000 --lr_drop 2000 ${PY_ARGS}
--- configs/DETReg_fine_tune_5pct_coco.sh
+++ configs/DETReg_fine_tune_5pct_coco.sh
@@ -5,4 +5,4 @@ set -x
 EXP_DIR=exps/DETReg_fine_tune_5pct_coco
 PY_ARGS=${@:1}
 
-python -u main.py --output_dir ${EXP_DIR} --filter_pct 0.05 --dataset coco --pretrain exps/DETReg_top30_coco/checkpoint.pth --epochs 500 ${PY_ARGS}
+python -u main.py --output_dir ${EXP_DIR} --filter_pct 0.05 --dataset coco --pretrain exps/DETReg_top30_coco/checkpoint.pth --epochs 500 --lr_drop 500 ${PY_ARGS}
